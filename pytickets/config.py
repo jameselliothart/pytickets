57fa71ba0c7e0c53142d46a2e07ed70fcbbf3a9a
James Hart <james.elliot.hart@gmail.com>
2021-06-05 00:09:00 +0000

    ensure secret key is set in prod

--- pytickets/config.py
+++ pytickets/config.py
@@ -1,10 +1,19 @@
 import os
 
+_DUMMY_SECRET = 'a-dummy-key'
+
+
+def _check_secret(secret, dummy_secret):
+    if secret == dummy_secret or len(secret) == 0:
+        raise ValueError('Must set PYTICKETS_SECRET_KEY')
+    return True
+
+
 class Config():
     ENVIRONMENT = 'BASE'
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.getenv('PYTICKETS_SECRET_KEY', 'a-dummy-key')
+    SECRET_KEY = os.getenv('PYTICKETS_SECRET_KEY', _DUMMY_SECRET)
 
 
 class TestConfig(Config):
@@ -19,3 +28,4 @@ class DevConfig(Config):
 
 class ProdConfig(Config):
     ENVIRONMENT = 'PROD'
+    _CHECK = _check_secret(os.getenv('PYTICKETS_SECRET_KEY'), _DUMMY_SECRET)
