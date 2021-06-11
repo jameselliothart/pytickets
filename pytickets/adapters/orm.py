40397f292647fd0aedc0225097b58bc851145804
James Hart <james.elliot.hart@gmail.com>
2021-06-11 22:26:06 +0000

    define orm

---
+++ pytickets/adapters/orm.py
@@ -0,0 +1,40 @@
+from uuid import uuid4
+from sqlalchemy import MetaData
+from sqlalchemy.orm import registry
+from sqlalchemy.sql.schema import Column, Table
+from sqlalchemy.sql.sqltypes import DateTime, String
+from sqlalchemy.dialects.postgresql import UUID
+import pytickets.tickets.domain as ticket
+
+mapper_registry = registry()
+
+ticket_table = Table(
+    'tickets',
+    mapper_registry.metadata,
+    Column('status', String(20)),
+    Column('id', UUID(as_uuid=True), primary_key=True, default=uuid4),
+    Column('created_on', DateTime()),
+    Column('updated_on', DateTime()),
+    Column('summary', String(75)),
+    Column('description', String(1000)),
+    Column('resolution', String(500)),
+    Column('completed_on', DateTime()),
+)
+
+
+def map():
+    mapper_registry.map_imperatively(ticket.Ticket,
+                                     ticket_table,
+                                     polymorphic_on='status',
+                                     polymorphic_identity='NotStarted',
+                                     )
+    mapper_registry.map_imperatively(ticket.InProgress,
+                                     ticket_table,
+                                     inherits=ticket.Ticket,
+                                     polymorphic_identity='InProgress'
+                                     )
+    mapper_registry.map_imperatively(ticket.Completed,
+                                     ticket_table,
+                                     inherits=ticket.Ticket,
+                                     polymorphic_identity='Completed'
+                                     )
