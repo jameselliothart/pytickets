b9b7e5a4ddc8f578926e6d9a1239d095ab764bff
James Hart <james.elliot.hart@gmail.com>
2021-06-13 21:40:13 +0000

    simplify with partial

--- pytickets/tickets/__init__.py
+++ pytickets/tickets/__init__.py
@@ -1,17 +1,18 @@
-from pytickets.tickets.handlers.sql import new_complete_ticket_handler, new_create_ticket_handler
+from pytickets.tickets.handlers.sql import complete_ticket_handler, create_ticket_handler
 from pytickets.config import get_datebase_uri
 from pytickets.adapters.orm import new_session_factory
 from flask import Blueprint
 from flask.templating import render_template
 from pytickets import oidc
+from functools import partial
 
 bp = Blueprint('tickets', __name__, template_folder='templates',
                url_prefix='/tickets')
 
 session_factory = new_session_factory(get_datebase_uri())
 
-create_ticket_handler = new_create_ticket_handler(session_factory)
-complete_ticket_handler = new_complete_ticket_handler(session_factory)
+create_ticket_handler =  partial(create_ticket_handler,session_factory)
+complete_ticket_handler = partial(complete_ticket_handler,session_factory)
 
 
 @bp.route('/dashboard')
--- pytickets/tickets/handlers/sql.py
+++ pytickets/tickets/handlers/sql.py
@@ -3,26 +3,20 @@ import pytickets.tickets.domain as ticket
 from sqlalchemy.orm import sessionmaker
 
 
-def new_create_ticket_handler(session_factory: sessionmaker):
-    def _create_ticket_handler(create_ticket: ticket.CreateTicket):
-        details = ticket.Details(
-            create_ticket.summary, create_ticket.description)
-        new_ticket = ticket.new(details)
-        with session_factory() as session:
-            session.add(new_ticket)
+def create_ticket_handler(session_factory: sessionmaker, create_ticket: ticket.CreateTicket):
+    details = ticket.Details(create_ticket.summary, create_ticket.description)
+    new_ticket = ticket.new(details)
+    with session_factory() as session:
+        session.add(new_ticket)
+        session.commit()
+    return ticket.TicketCreated(new_ticket)
+
+
+def complete_ticket_handler(session_factory: sessionmaker, cmd: ticket.CompleteTicket):
+    with session_factory() as session:
+        if t := session.query(ticket.Ticket).filter_by(id=cmd.id).first() is not None:
+            completed = ticket.complete(t, cmd.resolution)
+            session.query(ticket.Ticket).filter_by(id=cmd.id).update(
+                status=ticket.STATUSES[ticket.Completed], **asdict(completed))
             session.commit()
-        return ticket.TicketCreated(new_ticket)
-    return _create_ticket_handler
-
-
-def new_complete_ticket_handler(session_factory: sessionmaker):
-    def _complete_ticket_handler(cmd: ticket.CompleteTicket):
-        with session_factory() as session:
-            t = session.query(ticket.Ticket).filter_by(id=cmd.id).first()
-            if t is not None:
-                completed = ticket.complete(t, cmd.resolution)
-                session.query(ticket.Ticket).filter_by(id=cmd.id).update(
-                    status=ticket.STATUSES[ticket.Completed], **asdict(completed))
-                session.commit()
-        return ticket.TicketCompleted(completed)
-    return _complete_ticket_handler
+    return ticket.TicketCompleted(completed)
