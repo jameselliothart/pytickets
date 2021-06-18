87608b5d8005ed760cc0d8052274e745bb1b9c85
James Hart <james.elliot.hart@gmail.com>
2021-06-18 01:54:31 +0000

    complete ticket form

--- pytickets/tickets/forms.py
+++ pytickets/tickets/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from wtforms.fields.simple import HiddenField, SubmitField, TextAreaField
 from .domain import SCHEMA
 from wtforms import validators
 from wtforms.fields.core import StringField
@@ -9,3 +10,8 @@ class CreateTicketForm(FlaskForm):
         'Summary', [validators.Length(min=10, max=SCHEMA['summary'])])
     description = StringField(
         'Description', [validators.Length(max=SCHEMA['description'])])
+
+
+class CompleteTicketForm(FlaskForm):
+    resolution = TextAreaField('Resolution')
+    submit = SubmitField('Done!')
