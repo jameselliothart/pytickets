13502fd481388074803df4ca9bee07e027a2bb59
James Hart <james.elliot.hart@gmail.com>
2021-06-18 01:46:16 +0000

    query for open tickets

--- pytickets/tickets/handlers/queries.py
+++ pytickets/tickets/handlers/queries.py
@@ -1,7 +1,17 @@
 from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.sql.expression import select
+from pytickets.adapters.orm import ticket_table
 import pytickets.tickets.domain as ticket
 
 
 def get_all(session_factory: sessionmaker):
     with session_factory() as session:
         return session.query(ticket.Ticket).all()
+
+
+def get_open(session_factory: sessionmaker):
+    with session_factory() as session:
+        stmt = select(ticket_table).where(
+            ticket_table.c.status.notin_([ticket.STATUSES[ticket.Completed]])
+        )
+        return session.execute(stmt).all()
